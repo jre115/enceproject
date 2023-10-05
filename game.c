78d1fe4ba695fbafd5ad67e987604b977025db5c
Ruby Campbell-Stokes <rca128@uclive.ac.nz>
2023-10-06 12:46:34 +1300

    Turned blue LED to be on

--- game.c
+++ game.c
@@ -1,14 +1,21 @@
+#include <avr/io.h>
 #include "system.h"
 
 int main (void)
 {
     system_init ();
-
+    
+    /* Initialise port to drive LED 1.  */
+    DDRC |= (1 << 2); // set pin 2 to output
+    DDRD &= ~(1 << 7); // set bit 7 to be zero
 
     while (1)
     {
-
-
-
+        /* Turn LED 1 on.  */
+        if ((PIND & (1<<7)) != 0) {
+            PORTC |= (1 << 2); // turn on LED
+        } else {
+            PORTC &= ~(1 << 2); // turn off LED
+        }
     }
 }
