7569cfa72d67cc58bf0be5beb17bb88e1ec9ee44
Ruby Campbell-Stokes <rca128@uclive.ac.nz>
2023-10-11 19:22:34 +1300

    added changes from the transmission

--- game.c
+++ game.c
@@ -7,14 +7,17 @@
 #include "navswitch.h"
 #include "matrix.h"
 #include "nav.h"
+#include "ir_uart.h"
 
-#define PACER_RATE  500/* Pacer loop - controls main loop */
+#define PACER_RATE  500 /* Pacer loop - controls main loop */
 #define FROM_LOOP_EAST 1
 #define FROM_LOOP_WEST 2
 #define NUMBER_OF_CHOICES_FOR_START 2
 #define NUMBER_OF_CHOICES_FOR_ROUNDS 5
 
-
+#define MAXIMUM_ASCII_VALUE 265
+#define MINIMUM_ASCII_VALUE 0
+#define NUMBER_OF_ICONS 3
 /*
 void init_all(void)
 {
@@ -23,6 +26,7 @@ void init_all(void)
     pacer_init(PACER_RATE);
     navswitch_init ();
     init_text();
+    ir_uart_init();
 }
 
 */
@@ -48,6 +52,102 @@ void displayTutorial(void)
 */
 
 
+
+/*Displays welcome message and tutorial*/
+void game_Welcome(void)
+{
+    scrolling_text("Welcome to PSR! Move to start\0");
+    scrolling_text("View tutorial?\0");
+
+    char options[] = {'Y', 'N'};
+
+    char character = selectAndDisplayOptions(options, 2);
+
+    if (character == 'Y') {
+        displayTutorial();
+    }
+
+}
+
+void send_char(char character)
+{
+    /* TODO: Transmit the character over IR on a NAVSWITCH_PUSH event.  */
+    if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
+        ir_uart_putc(character);
+    }
+}
+
+
+char receive_char(char upperBound, char lowerBound){
+    char character = '0';
+    
+    if (ir_uart_read_ready_p()) {
+        char temp_char = ir_uart_getc();
+        if (temp_char >= lowerBound && temp_char <= upperBound) {
+            character = temp_char;
+        }
+    }
+    return character;
+}
+
+char set_num_rounds(void){
+    disp_text("How many rounds?\0");
+    char roundOptions[] = {'1', '3', '5', '7', '9'};
+    char numRounds = '0';
+    // display the choices
+    // bool roundsSet = false;
+    numRounds = selectVal(roundOptions, NUMBER_OF_CHOICES_FOR_ROUNDS);
+    // while (roundsSet == false) {
+    //     if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
+    //         // set to user of this boards value
+    //         numRounds = selectVal(roundOptions, NUMBER_OF_CHOICES_FOR_ROUNDS);
+    //         roundsSet = true;
+    //     } else if (ir_uart_read_ready_p()) {
+    //         numRounds = receiveChar('1', '9');
+    //         // got to check if it's 2, 3, 6 or 8
+    //         // set to this value
+    //         displayReceivedChar(numRounds);
+    //     }
+    // }
+    return numRounds;
+}
+
+// char setup_game(void)
+// {
+//     return setNumRounds(); // getting the rounds from either this player or the other one
+
+// }
+
+void rotate_through_icons(void) 
+{
+    int16_t button_tick = 0;
+    while(1) {
+        pacer_wait();
+        button_tick++;
+        if ((button_tick - 60) > 0) {
+            matrix_init();
+            break;
+        } else if ((button_tick - 45) > 0) {
+            tinygl_text("Shoot\0");
+        } else if ((button_tick - 30) > 0) {
+            disp_rock();
+        } else if ((button_tick - 15) > 0) {
+            disp_scissors();
+        } else {
+            disp_paper();
+        }
+    }
+    
+}
+
+void game_start(char roundsChar)
+{
+    uint8_t rounds = roundsChar - '0';
+    for (uint8_t i = 0; i < rounds; i++) {
+        // play a game of paper sissors rock and display winner
+        rotateThroughIcons();
+    }
+}
 
 
 
@@ -94,27 +194,18 @@ void scrolling_text(char* text)
 }
 
 
-void welcome_game(void)
-{
-    scrolling_text("Welcome to PSR! Move to start\0");
-    scrolling_text("View tutorial?\0");
 
-    char options[] = {'Y', 'N'};
-
-    char character = selectAndDisplayOptions(options, 2);
-
-    //if (character == 'Y') {
-        //displayTutorial();
-    //}
-
-}
 
 
 int main (void)
 {
+    init_all();
     pacer_init(PACER_RATE);
 
-    welcome_game();
+    game_welcome();
+    
+    char numRounds = set_num_rounds();
+    game_start(numRounds);
 
     //start_game();
     //setup_game();
