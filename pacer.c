5413ebaa24cc22e7070687fe97db031f6199d392
Jordan Redfern <jre115@uclive.ac.nz>
2023-10-18 08:06:20 +1300

    final push!

--- pacer.c
+++ pacer.c
@@ -1,36 +1,36 @@
+/*
+# File:   displays.c
+# Author: J. Redfern, R. Campbell-Stokes
+# Date:   18 October 2023
+# Descr:  Contains logic for the pacer function
+*/
+
 #include <avr/io.h>
 #include "pacer.h"
 
 static uint16_t pacer_period;
 
-/* Initialise the pacer module.  */
+
+/* Initialise the pacer module with specified frequency  */
 void pacer_init (uint16_t pacer_frequency)
 {
-    /* TODO: initialise timer/counter peripheral the
-       same way as in lab2/lab2-ex2/timer.c but also calculate 
-       the timer/counter value from the pacer frequency */
-
+    // Set Timer/Counter 1 Control Registers
     TCCR1A = 0x00;
     TCCR1B = 0x05;
     TCCR1C = 0x00;
 
+    // Calculate delay and pacer period
     uint16_t delay = (((1 * 1000) / pacer_frequency) / 2); 
-
-    uint32_t fcpu = 8000000;
-    uint16_t prescaler = 1024; /*2^10 = 1024*/
     pacer_period = (delay * (fcpu / prescaler) / 1000);
-
 }
 
-
-/* Pace a while loop.  */
+/*The function waits for the specified period using the pacer.
+ It uses Timer/Counter 1 to generate a delay based on the pacer period.*/
 void pacer_wait (void)
 {
-
-    /* TODO: Implement the same way as the timer_delay () function 
-       lab2-except reset TCNT1 after the while loop. */
     while (TCNT1 < (pacer_period)) {
         continue;
     }
-    TCNT1=0;
+    // resets after the while loop
+    TCNT1 = 0;
 }
--- pacer.h
+++ pacer.h
@@ -1,13 +1,24 @@
+/*
+# File:   displays.h
+# Author: J. Redfern, R. Campbell-Stokes
+# Date:   18 October 2023
+# Descr:  Contains logic for the pacer function
+*/
+
 #ifndef PACER_H
 #define PACER_H
 
 #include "system.h"
 
-/* Initialise the pacer module.  */
+#define fcpu 8000000
+#define prescaler 1024
+
+/* Initialise the pacer module with specified frequency  */
 void pacer_init (uint16_t pacer_frequency);
 
 
-/* Pace a while loop.  */
+/*The function waits for the specified period using the pacer.
+ It uses Timer/Counter 1 to generate a delay based on the pacer period.*/
 void pacer_wait (void);
 
 #endif //PACER_H
